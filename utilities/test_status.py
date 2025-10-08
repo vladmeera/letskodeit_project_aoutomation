53b0e26a91653f7eb1707694e51ff2f1f17c1364
vmeergo <migorodvld@gmail.com>
2025-10-07 22:02:56 -0400

    Make a few changes

--- utilities/test_status.py
+++ utilities/test_status.py
@@ -19,27 +19,15 @@ class StatusOfTest(SeleniumDriver):
             self.log.info(result)
             if result is not None and result:
                 self.result_list.append("Pass")
-
                 self.log.info(f"--> --> --> | TEST {test_number}/{total_tests} SUCCESSFUL | {result_message_success} <-- <-- <--")
-                self.log.info(f"------------------------------------------------------------------------------------------------")
-                self.log.info(f"   ")
-
             else:
                 self.result_list.append("Fail")
-
                 self.log.info(f"->> ->> ->> ->> | TEST {test_number}/{total_tests} FAIL | {result_message_fail} <<- <<- <<- <<-")
-                self.log.info(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                self.log.info(f"   ")
-
                 self.screenshot(result_message_fail)
 
         except Exception as e:
             self.result_list.append("Fail")
-
-            self.log.error(f"************************************************************************")
             self.log.error(f"-------------------| EXCEPTION OCCURRED | {e} |------------")
-            self.log.error(f"************************************************************************")
-
             self.screenshot(result_message_fail)
 
     def mark(self, test_number, total_tests, result, result_message_success = " ", result_message_fail=" "):
@@ -57,17 +45,11 @@ class StatusOfTest(SeleniumDriver):
         self.set_result(test_number, total_tests, result, result_message_success, result_message_fail)
 
         if "Fail" in self.result_list:
-            self.log.error(f"********************************************************************************")
             self.log.error(f"-------------------| {test_name} | TEST FAILED |------------")
-            self.log.error(f"********************************************************************************")
-
             self.result_list.clear()
             assert True == False
 
         else:
-            self.log.info(f"----------------------------------------------------------------------")
             self.log.info(f"--> --> --> | {test_name} | TEST SUCCESSFUL | <-- <-- <--")
-            self.log.info(f"----------------------------------------------------------------------")
-
             self.result_list.clear()
             assert True == True
--- utilities/util.py
+++ utilities/util.py
@@ -8,7 +8,7 @@ Example:
     name = self.util.get_unique_name()
 """
 try:
-    from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits
+    from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits, printable
     import time
     import traceback
     import random
@@ -23,6 +23,7 @@ class Util(object):
     log = cl.custom_logger(DEBUG)
 
     def sleep(self, seconds, message = "???") -> None:
+        assert seconds > 0, "Seconds can't be negative or 0"
         """
         Put the program to wait for the specified time
         """
@@ -35,11 +36,12 @@ class Util(object):
             self.log.error(f"| INTERRUPTED ERROR | {e}")
             traceback.print_stack()
 
-    def get_alpha_numeric(self, length, char_type) -> str:
+    def get_alpha_numeric(self, length: int, char_type: str = 'letters', is_password: bool = False) -> str | None:
         """
         Get random string of characters
 
         Parameters:
+            is_password: False by default
             length: Length of string, number of characters string should contain
             char_type: Type of characters string should contain, default is "letters"
             Provide lower/upper/digits/mix for different types
@@ -51,61 +53,77 @@ class Util(object):
             'upper': ascii_uppercase,
             'digits': digits,
             'mix': ascii_letters + digits,
-            'letters': ascii_letters
+            'letters': ascii_letters,
+            'printable': printable
         }
-        case = char_type_dict[char_type]
+        if is_password:
+            char_type = 'printable'
+            case = char_type_dict.get(char_type)
+            return alpha_num.join(random.choice(case) for i in range(length))
+        else:
+            case = char_type_dict.get(char_type.lower())
+            if case is None:
+                self.log.warning(f"There is no - {char_type} - character type!")
+            else:
+                return alpha_num.join(random.choice(case) for i in range(length))
 
-        return alpha_num.join(random.choice(case) for i in range(length))
+    def get_password(self, length: int = 1) -> str:
+        assert length > 0, "Can't be less than 0"
+        assert length <= 64, "Sorry, but it can't be more than 64 characters!"
 
-    def get_unique_name(self, char_count: int = 5):
+        return self.get_alpha_numeric(length=length, is_password=True)
+
+
+    def get_unique_name_lower(self, char_count: int = 5):
+        assert char_count > 0, "Char_ count can't be less than 1"
         """
-        Get unique name
+        Get unique name in lower case
 
         :param char_count: amount of characters
         :return: unique name as string
         """
         return self.get_alpha_numeric(char_count, char_type="lower")
 
-    def verify_text_contains(self, actual_text, expected_text):
+    def get_unique_name_upper(self, char_count: int = 5) -> str:
+        assert char_count > 0, "Char_ count can't be less than 1"
+        """
+        Get unique name in upper case
+
+        :param char_count: amount of characters
+        :return: unique name as string
+        """
+        return self.get_alpha_numeric(char_count, char_type="upper")
+
+    def verify_text_contains(self, actual_text: str, expected_text: str) -> bool | None:
+
         """
         Verify actual text contains expected text
 
         :return: True/False
         """
-        self.log.info(f"------------------------------------------------------------------------")
-        self.log.info(f"---------------------| VERIFYING TEXT |---------------------------------")
-
         self.log.info(f"--> Actual text from application --> | {actual_text} |")
-        self.log.info(f"->> Expected text from test case ->> | {expected_text} |")
-        if expected_text.lower() in actual_text.lower():
-            self.log.info(f"{"-" * 10}VERIFICATION PASSED --> ACTUAL TEXT CONTAINS EXPECTED TEXT{"-" * 10}")
-            self.log.info(f"------------------------------------------------------------------------")
+        self.log.info(f"->> Expected text to be in actual ->> | {expected_text} |")
+        if expected_text.lower() in actual_text.lower() and (expected_text != '' and expected_text != ' '):
+            self.log.info(f"VERIFICATION PASSED! The actual text is in the expected text!")
             return True
-        else:
-            self.log.warning(
-                f"{"*" * 10}VERIFICATION FAILED --> ACTUAL TEXT DOES NOT CONTAIN EXPECTED TEXT{"*" * 10}")
-            self.log.info(f"------------------------------------------------------------------------")
-
+        elif expected_text.lower() not in actual_text.lower() and (actual_text != '' and actual_text != ' '):
+            self.log.warning(f"VERIFICATION FAILED! The actual text is not in the expected text!")
             return False
+        else:
+            self.log.warning("Verification was not successful! Actual or expected text can't be empty!")
 
-    def verify_text_match(self, actual_text, expected_text):
+    def verify_text_match(self, actual_text, expected_text) -> bool:
         """
         Verify actual text matches with expected text
 
         :return: True/False
         """
-        self.log.info(f"------------------------------------------------------------------------")
-        self.log.info(f"---------------------| VERIFYING TEXT |---------------------------------")
-
-        self.log.info(f"--> Actual text from application --> | {actual_text} |")
-        self.log.info(f"->> Expected text from test case --> | {expected_text} |")
+        self.log.info(f"--> Actual text from the application --> | {actual_text} |")
+        self.log.info(f"->> Expected text from the test case --> | {expected_text} |")
         if actual_text.lower() == expected_text.lower():
             self.log.info(f"{"-" * 10}VERIFICATION PASSED --> ACTUAL TEXT CONTAINS EXPECTED TEXT{"-" * 10}")
-            self.log.info(f"------------------------------------------------------------------------")
             return True
         else:
             self.log.warning(
                 f"{"*" * 10}VERIFICATION FAILED --> ACTUAL TEXT DOES NOT CONTAIN EXPECTED TEXT{"*" * 10}")
-            self.log.info(f"------------------------------------------------------------------------")
-
             return False
